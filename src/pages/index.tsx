9daeedc987822c24df7b722a468f866b2f769305
ainochi-kor <kthrkdals@gmail.com>
2025-05-13 23:57:41 +0900

    서버 사이드에서 도서 목록 및 추천 도서를 가져오는 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,42 @@
 import SearchableLayout from "@/components/searchable-layout";
 import styles from "./index.module.css";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-itme";
+import fetchBooks from "@/lib/fetch-books";
+import { InferGetServerSidePropsType } from "next";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    props: {
+      allBooks,
+      recoBooks,
+    },
+  };
+};
+
+export default function Home({
+  allBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(allBooks);
   return (
     <div className={styles.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
-        ))} 
+        ))}
       </section>
     </div>
   );
